c1093ada7ac308dbf21c20b18f0f6ccb5b74dfa5
wangyi2005 <wangyi2005@users.noreply.github.com>
2020-01-02 21:23:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 cd /brook
-wget -O https://github.com/txthinking/brook/releases/download/v$VER/brook
+wget https://github.com/txthinking/brook/releases/download/v$VER/brook
 chmod +x brook
 ./brook wsserver -l :$PORT -p $PASSWORD
