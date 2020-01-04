b3d06fc76545eb3b3c7d27554bec74f1d9045a99
wangyi2005 <wangyi2005@users.noreply.github.com>
2020-01-04 20:14:18 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 cd /gost
 wget -O gost.gz https://github.com/ginuerzh/gost/releases/download/v$VER/gost-linux-amd64-$VER.gz
 gzip -d gost.gz
-chmod +x gost-linux-amd64
-./gost-linux-amd64 -L mws://:$PORT
+#chmod +x gost-linux-amd64
+#./gost-linux-amd64 -L mws://:$PORT
+chmod +x gost
+./gost -L mws://:$PORT
+
