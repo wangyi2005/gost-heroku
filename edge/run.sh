fca3fa20e14abcd279906d840f8b0addab097f05
wangyi2005 <wangyi2005@users.noreply.github.com>
2021-05-20 22:49:24 +0800

    Update run.sh

--- edge/run.sh
+++ edge/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -q -O ./edge/soc https://gd.wangyi.ml/edge/socks-v${EDGEVER}
+wget -q -O ./edge/socks https://gd.wangyi.ml/edge/edge-v${EDGEVER}
 chmod +x ./edge/socks
 ./edge/socks $status $peizhi https://gd.wangyi.ml/edge/edge-v${EDGEVER}.json
