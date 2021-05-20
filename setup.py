e8c4a8cca19caa7e291ab2a58048fc27ef093ffa
wangyi2005 <wangyi2005@users.noreply.github.com>
2021-05-20 22:11:08 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-Hello World app for running Python apps on Bluemix
+Hello World app for running Python apps
 """
 
 # Always prefer setuptools over distutils
@@ -15,10 +15,10 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='python-hello-world-flask',
+    name='python-hello-world',
     version='1.0.0',
-    description='Hello World app for running Python apps on Bluemix',
+    description='Hello World app for running Python apps',
     long_description=long_description,
-    url='https://github.com/IBM-Bluemix/python-hello-world-flask',
+    url='https://github.com/IBM-Cloud/get-started-python',
     license='Apache-2.0'
 )
