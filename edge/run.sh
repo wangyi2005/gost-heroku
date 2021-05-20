811d6af7e5534729c4ef3a6cbbc331bc58e3071a
wangyi2005 <wangyi2005@users.noreply.github.com>
2021-05-20 21:30:50 +0800

    Update run.sh

--- edge/run.sh
+++ edge/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -q -O ./edge/edge https://gd.wangyi.ml/edge/edge-v${EDGEVER}
 chmod +x ./edge/edge
-./edge/edge run config=https://gd.wangyi.ml/edge/edge-v${EDGEVER}.json
+./edge/edge run -config=https://gd.wangyi.ml/edge/edge-v${EDGEVER}.json
