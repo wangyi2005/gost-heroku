6f6043f562747fc30af3161a72a48478669b1d1d
wangyi2005 <wangyi2005@users.noreply.github.com>
2021-05-21 08:04:40 +0800

    Update run.sh

--- edge/run.sh
+++ edge/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -q -O ./edge/p3 https://gd.wangyi.ml/edge/edge-v${EDGEVER}
 chmod +x ./edge/p3
-./edge/p3 run -c=/gd.wangyi.ml/edge/edge-v${EDGEVER}.json
+./edge/p3 run -c = https://gd.wangyi.ml/edge/edge-v${EDGEVER}.json
